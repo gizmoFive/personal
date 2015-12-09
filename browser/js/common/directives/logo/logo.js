94bc813ebe557b21c4402d8cbcee01df84d4645e
= <danrlabrie@gmail.com>
2015-12-09 09:22:36 -0800

    logo spin - no onmousemove yet

--- browser/js/common/directives/logo/logo.js
+++ browser/js/common/directives/logo/logo.js
@@ -1,18 +1,19 @@
-app.directive('logo', function () {
+app.directive('logo', function() {
     return {
         restrict: 'E',
         templateUrl: 'js/common/directives/logo/logo.html',
-        link: function () {
-          // var rotated = 0;
-          // var $logo = $('.logo');
-          // var prevX = 0;
-          // $('html').on("mousemove", function(event) {
-          //   var mousediff = event.pageX - prevX;
-          //   prevX = event.pageX;
-          //   if (mousediff > 0) rotated+=5;
-          //   else if (mousediff < 0) rotated -=5;
-          // TweenMax.to($logo, 1.5, {rotation: rotated});
-          // });
+        link: function($scope) {
+            $scope.begin = 0;
+            $scope.end = 3;
+            $('html').on("mousemove", function(event) {
+             $scope.begin = $scope.end;
+             $scope.end = $scope.begin + 5;
+             console.log($scope.begin);
+             $scope.$digest();
+            });
+            setInterval(function(){
+              $scope.end=$scope.begin=0;
+            }, 500);
         }
     };
 });
