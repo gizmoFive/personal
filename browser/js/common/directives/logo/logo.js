43201c66209c77dfcc24f21db082cf8c7e0542c4
= <danrlabrie@gmail.com>
2016-01-19 11:35:32 -0800

    minor fixes

--- browser/js/common/directives/logo/logo.js
+++ browser/js/common/directives/logo/logo.js
@@ -2,19 +2,6 @@ app.directive('logo', function() {
     return {
         restrict: 'E',
         templateUrl: 'js/common/directives/logo/logo.html',
-        // link: function($scope) {
-        //     $scope.begin = 0;
-        //     $scope.end = 3;
-        //     $('html').on("mousemove", function(event) {
-        //      $scope.begin = $scope.end;
-        //      $scope.end = $scope.begin + 5;
-        //      console.log($scope.begin);
-        //      $scope.$digest();
-        //     });
-        //     setInterval(function(){
-        //       $scope.end=$scope.begin=0;
-        //     }, 500);
-        // }
     };
 });
 
