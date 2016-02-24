7ab7e5f8b7892c4504cee4540c2e0d0555ffd5b3
= <danrlabrie@gmail.com>
2016-02-24 10:04:01 -0800

    finish gol presets

--- browser/js/code/computerScience/gol/gol.js
+++ browser/js/code/computerScience/gol/gol.js
@@ -14,7 +14,7 @@ app.config(function ($stateProvider) {
 });
 
 
-app.controller('golctrl', function ($scope) {
+app.controller('golctrl', function ($scope, $q) {
 
 var hasrun = false;
 $scope.playval = 'Play';
@@ -212,10 +212,11 @@ var gameOfLife = {
     }
     $scope.$digest();
 },
-  loadpreset: function(name) {
+  loadpreset: function(arr) {
+        if(arr) {
         this.clearBoard();
         this.forEachCell(function(cell) {
-      if (name.indexOf(cell.id) !== -1) {
+      if (arr.indexOf(cell.id) !== -1) {
         cell.className = "alive";
         cell.setAttribute('data-status', 'alive');
       } else {
@@ -224,9 +225,25 @@ var gameOfLife = {
       }
     });
     }
+    }
 
 };
-
+var presetObj = {
+    'Gosper Glider Gun' : [45,45,'4-10','5-10','4-11','5-11','14-10','14-11','14-12','15-9','15-13','16-8','17-8','16-14','17-14','18-11','19-9','19-13','20-10','20-11','20-12','21-11','24-8','24-9','24-10','25-8','25-9','25-10','26-7','26-11','28-6','28-7','28-11','28-12','38-8','38-9', '39-8', '39-9'],
+    '10-cell Stable' : [25,25,'7-12','8-12','9-12','10-12','11-12','12-12','13-12','14-12','15-12','16-12'],
+    'Flippers' : [25,25,'4-3','5-3','3-3','1-5','1-6','1-7','7-5','7-6','7-7','3-9','4-9','5-9','17-5','18-5','19-5','15-7','15-8','15-9','21-7','21-8','21-9','17-11','18-11','19-11','10-11','10-12','10-13','10-17','10-18','10-19','6-15','7-15','8-15','12-15','13-15','14-15'],
+    'Glider Collider' : [29,26,'1-0','27-0','2-1','26-1','0-2','1-2','2-2','26-2','27-2','28-2','0-23','1-23','2-23','26-23','27-23','28-23','2-24','26-24','1-25','27-25']
+};
+$scope.$watch('preset', function() {
+    var mypromise = new Promise(function (resolve, reject) {
+    var pre = presetObj[$scope.preset];
+    $scope.golwidth = pre[0];
+    $scope.golheight = pre[1];
+    resolve(pre);
+}).then(function (pre) {
+    gameOfLife.loadpreset(pre);
+});
+});
 $scope.playspeed = 2;
 $scope.$watch('playspeed', function(){
     
