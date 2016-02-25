76ad3cccacdc03c92969de7b051f5a2c3cb69b7b
= <danrlabrie@gmail.com>
2016-02-25 09:24:57 -0800

    minor code state changes + seo

--- browser/js/code/code.js
+++ browser/js/code/code.js
@@ -14,7 +14,7 @@ app.config(function ($stateProvider) {
 });
 
 
-app.controller('codectrl', function ($scope) {
+app.controller('codectrl', function ($scope, $rootScope, $state, $q) {
   $scope.interests = [
     {name: 'computerScience',
      'url' : 'http://s9.postimg.org/av72kc5wv/code_2_256.png',
@@ -22,12 +22,12 @@ app.controller('codectrl', function ($scope) {
     {name: 'd3',
      url : 'https://raw.githubusercontent.com/d3/d3-logo/master/d3.png',
      message: 'Data Vis Stuff'},
-    {name: 'react',
-     url : 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/React.js_logo.svg/2000px-React.js_logo.svg.png',
-     message: 'React / Flux / Redux'},
     {name: 'angular',
      url : 'https://angularjs.org/img/ng-logo.png',
      message: 'Angular'},
+     {name: 'react',
+     url : 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/React.js_logo.svg/2000px-React.js_logo.svg.png',
+     message: 'React / Flux / Redux'},
     {name: 'node',
      url : 'https://nodejs.org/static/images/logos/nodejs-new-white-pantone.png',
      message: 'Back End Stuff'}
@@ -42,6 +42,16 @@ $scope.mouseenterfn = function () {
 
 $scope.mouseleavefn = function () {
     this.interest.url = placeholder;
+};
+
+$scope.runfunction = function() {
+        var that = this;
+        $('#' + that.interest.name).fadeOut('slow', $q(function(resolve, reject) {
+                resolve(that);
+                }).then(function(that) {
+                    $state.go(that.interest.name);
+                }));
+
 };
 
 });
