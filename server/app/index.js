ab2bf271d14d14561af04e1cd9c955a8e8b5080c
= <danrlabrie@gmail.com>
2016-04-26 22:38:29 -0700

    optimization

--- server/app/index.js
+++ server/app/index.js
@@ -2,12 +2,14 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var compression = require('compression');
 module.exports = app;
 
 // Pass our express application pipeline into the configuration
 // function located at server/app/configure/index.js
 require('./configure')(app);
 
+app.use(compression())
 // Routes that will be accessed via AJAX should be prepended with
 // /api so they are isolated from our GET /* wildcard.
 app.use('/api', require('./routes'));
